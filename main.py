7edb2d6ca33f2f5c5e1888807fef2fdb3f7a4d4a
Oydar <oydar1@outlook.com>
2025-02-04 01:20:13 +0300

    add factory pattern to process model's @classmethod

--- main.py
+++ main.py
@@ -3,14 +3,5 @@ from src.models.process import Process
 
 if __name__ == "__main__":
     for proc in psutil.process_iter(['pid', 'name', 'exe', 'status']):
-        if proc.pid == 0:
-            continue
-        try:
-            process = Process(proc.pid, proc.name(), proc.exe(), proc.status())
-            print(process.get_edited_pid(), process.get_edited_name(), process.get_path(), process.get_edited_status())
-        except psutil.AccessDenied:
-            print(f"Access denied for PID {proc.pid} ({proc.name()})")
-        except psutil.NoSuchProcess:
-            pass
-
-
+        process = Process.from_psutil_proc(proc)
+        print(process)
--- src/models/process.py
+++ src/models/process.py
@@ -7,13 +7,21 @@ class Process:
         self._name = name
         self._path = path
         self._status = status
-        self._status_running = 0
-        self.status_stopped = 0
-        self._status_waiting = 0
-        self._status_dead = 0
-        self._status_sleeping = 0
-        self._status_zombie = 0
         init()
+
+    def __str__(self):
+        return f'{self.get_edited_pid()} {self.get_edited_name()} {self.get_path()} {self.get_edited_status()}'
+
+    @classmethod
+    def from_psutil_proc(cls, proc):
+        if proc.pid == 0:
+            return None
+        try:
+            return cls(proc.pid, proc.name(), proc.exe(), proc.status())
+        except psutil.AccessDenied:
+            print(f"Access denied for PID {proc.pid} ({proc.name()})")
+        except psutil.NoSuchProcess:
+            return None
 
     def get_edited_pid(self):
         return Fore.BLACK + Back.WHITE + f'{self._pid}:' + Style.RESET_ALL
@@ -26,24 +34,14 @@ class Process:
 
     def get_edited_status(self):
         if self._status == psutil.STATUS_RUNNING:
-            self._status_running += 1
             return Fore.GREEN + self._status + Style.RESET_ALL
         elif self._status == psutil.STATUS_WAITING:
-            self._status_waiting += 1
             return Fore.YELLOW + self._status + Style.RESET_ALL
         elif self._status == psutil.STATUS_STOPPED:
-            self.status_stopped += 1
             return Fore.RED + self._status + Style.RESET_ALL
         elif self._status == psutil.STATUS_DEAD:
-            self._status_dead += 1
             return Fore.RED + self._status + Style.RESET_ALL
         elif self._status == psutil.STATUS_SLEEPING:
-            self._status_sleeping += 1
             return Fore.YELLOW + self._status + Style.RESET_ALL
         elif self._status == psutil.STATUS_ZOMBIE:
-            self._status_zombie += 1
             return Fore.CYAN + self._status + Style.RESET_ALL
-
-    def get_status_counter(self):
-        running =  Fore.GREEN + "Running: " + Style.RESET_ALL
-        running + str(self._status_running)
