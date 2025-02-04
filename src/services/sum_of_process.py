103efeec808282e24226abc0d9cc337618272de4
Oydar <oydar1@outlook.com>
2025-02-04 19:37:13 +0300

    1.0.1 fix struct of project, refactor code

--- src/services/sum_of_process.py
+++ src/services/sum_of_process.py
@@ -8,16 +8,19 @@ def sum_of_process():
 
     for pid in pids:
         proc = psutil.Process(pid)
+        status = proc.status()
 
-        if proc.status() == psutil.STATUS_RUNNING:
+        if status == psutil.STATUS_RUNNING:
             process_counter.increment_running()
-        elif proc.status() == psutil.STATUS_WAITING:
+        elif status == psutil.STATUS_STOPPED:
+            process_counter.increment_stopped()
+        elif status== psutil.STATUS_WAITING:
             process_counter.increment_waiting()
-        elif proc.status() == psutil.STATUS_DEAD:
+        elif status == psutil.STATUS_DEAD:
             process_counter.increment_dead()
-        elif proc.status() == psutil.STATUS_SLEEPING:
+        elif status == psutil.STATUS_SLEEPING:
             process_counter.increment_sleeping()
-        elif proc.status() == psutil.STATUS_ZOMBIE:
+        elif status == psutil.STATUS_ZOMBIE:
             process_counter.increment_zombie()
 
     print(process_counter)
